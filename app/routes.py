86576d506b6ae352df02e02b664117ca27047332
Denis BARON <denis.baron@cnes.fr>
2022-10-17 17:11:17 +0200

    Form showltt

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,9 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app
 
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/showltt')
+def showltt():
+    return render_template('showltt.html')
 
 
 @app.route("/")
